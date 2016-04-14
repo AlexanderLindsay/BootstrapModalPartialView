9701610a3dd1a95ed84710051f7c5427c2080d1a
Alexander Lindsay <aml.lindsay@gmail.com>
2016-04-13 20:11:37 -0700

    added a asp.net core project and created the form modal sample in it for reference

---
+++ BootstrapModalPartialViewCore/Models/CloseModal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapModalPartialViewCore.Models
+{
+    public class CloseModal
+    {
+        public bool ShouldClose { get; set; }
+        public bool FetchData { get; set; }
+        public string Destination { get; set; }
+        public string Target { get; set; }
+        public string OnSuccess { get; set; }
+    }
+}
